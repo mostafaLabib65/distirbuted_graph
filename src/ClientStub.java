1c1415ba8496f2994fb2739d96df0b7d1a212ec1
Mahmoud Gamal <mahmoud.gamal.15497@gmail.com>
2020-03-19 14:35:45 +0200

    added client stub

---
+++ src/ClientStub.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface ClientStub extends Remote {
+
+    ArrayList<String> execute(ArrayList<String> commands) throws RemoteException;
+
+}
